af8a4910ef5fdfada176a52137eb9553e5ab4fe1
parmardeepak <103151865+parmardeepak@users.noreply.github.com>
2024-12-22 18:42:42 -0500

    Update main.go

--- 06-variables/04-assignment/exercises/02-vars-to-vars/main.go
+++ 06-variables/04-assignment/exercises/02-vars-to-vars/main.go
@@ -38,13 +38,14 @@ package main
 func main() {
 	// UNCOMMENT THE CODE BELOW:
 
-	// color := "green"
+	color := "green"
 
-	// ADD YOUR CODE BELOW
+	ADD YOUR CODE BELOW
 
-	// ?
+	//?
+	color:=dark+color
 
-	// UNCOMMENT THE CODE BELOW TO PRINT THE VARIABLE
+	//UNCOMMENT THE CODE BELOW TO PRINT THE VARIABLE
 
-	// fmt.Println(color)
+	fmt.Println(color)
 }
